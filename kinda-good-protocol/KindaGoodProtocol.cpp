2ba61df38b02a9bc8114605a2f491aa3f072c9da
Krixium <bennywang75@hotmail.com>
2018-11-27 13:58:58 -0800

    Fixed issue with writing to file

--- kinda-good-protocol/KindaGoodProtocol.cpp
+++ kinda-good-protocol/KindaGoodProtocol.cpp
@@ -11,7 +11,7 @@ KindaGoodProtocol::KindaGoodProtocol(QWidget *parent)
 
 	// TESTING ONLY
 	mFile = new QFile("dump.txt");
-	mFile->open(QIODevice::WriteOnly | QIODevice::Text);
+	mFile->open(QIODevice::WriteOnly);
 	Q_ASSERT(mFile->isOpen() && mFile->isWritable());
 	// END
 
