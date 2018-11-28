c68117e39e633419e3077ea160c0959ffe4ae174
Krixium <bennywang75@hotmail.com>
2018-11-27 17:00:45 -0800

    Changed output file name and added delay to network emulator

--- network-emu.py
+++ network-emu.py
@@ -1,36 +1,70 @@
+from collections import deque
+from threading import Thread
 import random
 import socket
 import sys
+import time
 
+def incoming(s, q, d):
+    # infinie run loop
+    while True:
+        # read a packet
+        payload, incoming_client = s.recvfrom(1504)
+        print "Incoming packet from", incoming_client
+        # if the packet is not being droppped, queue it
+        if float(random.random()) > float(loss_rate):
+            q.append((payload, incoming_client, time.time() + int(d)))
+
+
+def outgoing(s, q, clients):
+    # infinite run loop
+    while True:
+        while len(q) > 0:
+            x = q.popleft()
+            payload = x[0]
+            client = x[1]
+            out_time = x[2]
+            if time.time() > out_time:
+                if client == clients[0]:
+                    s.sendto(payload, clients[1])
+                elif client == clients[1]:
+                    s.sendto(payload, clients[0])
+                else:
+                    print "Error"
+            else:
+                q.appendleft(x)
+                break
+
+
+# variables
 server = ("0.0.0.0", 8000)
 clients = [("192.168.0.18", 8000), ("192.168.0.233", 8000)]
 loss_rate = 0
+delay = 0
+packet_queue = deque()
 
+# getting user input
 argc = len(sys.argv)
 
-if argc == 2:
+if argc >= 2:
     loss_rate = sys.argv[1]
 
+if argc >= 3:
+    delay = sys.argv[2]
+
+
+# start
 print "Loss rate:", loss_rate
 
 # create the socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(server)
 
-# infinie run loop
-while (True):
-    # read a packet
-    payload, incoming_client = s.recvfrom(1504)
-    print "Incoming packet from", incoming_client
+incomingThread = Thread(target=incoming, args=(s, packet_queue, delay,))
+outgoingThread = Thread(target=outgoing, args=(s, packet_queue, clients,))
 
-    # if packet was not randomly droppped
-    if float(random.random()) > float(loss_rate):
-        # send packet to correct host
-        if incoming_client == clients[0]:
-            s.sendto(payload, clients[1])
-            print "Forwarding to", clients[1]
-        elif incoming_client == clients[1]:
-            s.sendto(payload, clients[0])
-            print "Forwarding to", clients[0]
-        else:
-            print "Address error"
+incomingThread.start()
+outgoingThread.start()
+
+incomingThread.join()
+outgoingThread.join()
